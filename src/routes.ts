b1fb3592441510a6a59170960d27dab5634e58ab
Gabriel Gomes de Almeida <gabrielda@.ciandt.com>
2022-05-09 10:29:00 -0300

    Pre deployment adjustments - try/catch

--- src/routes.ts
+++ src/routes.ts
@@ -12,6 +12,8 @@ routes.post('/feedbacks', async (req, res) => {
 
   const { type, comment, screenshot } = req.body
 
+
+  try{
   const prismaFeebacksRepository = new PrismaFeedbacksRepository()
   const nodemailerMailAdapter = new NodeMailerMailAdapter()
 
@@ -28,4 +30,10 @@ routes.post('/feedbacks', async (req, res) => {
   })
 
   return res.status(201).send()
+
+} catch (err) {
+  console.log(err)
+
+  return res.status(500).send()
+}
 })
